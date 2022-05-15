96c59ea3ee51c0860bd2e12b4ea1208da3bfb2b3
root <QQGoblin>
2022-05-15 19:44:06 +0800

    kk

--- code/chapter-3/bpf_fs/map_pinning_save.c
+++ code/chapter-3/bpf_fs/map_pinning_save.c
@@ -2,19 +2,22 @@
 #include <linux/bpf.h>
 #include <stdio.h>
 #include <string.h>
-#include "bpf.h"
+#include <bpf/bpf_helper_defs.h>
+#include <bpf/bpf.h>
 
 static const char *file_path = "/sys/fs/bpf/my_array";
 
 int main(int argc, char **argv) {
   int key, value, fd, added, pinned;
 
+  // 创建 BPF_MAP_TYPE_ARRAY 类型的字典
   fd = bpf_create_map(BPF_MAP_TYPE_ARRAY, sizeof(int), sizeof(int), 100, 0);
   if (fd < 0) {
     printf("Failed to create map: %d (%s)\n", fd, strerror(errno));
     return -1;
   }
 
+  // 写入数据
   key = 1, value = 1234;
   added = bpf_map_update_elem(fd, &key, &value, BPF_ANY);
   if (added < 0) {
@@ -22,6 +25,7 @@ int main(int argc, char **argv) {
     return -1;
   }
 
+  // 绑定到指定路径，提供给用户空间程序查询
   pinned = bpf_obj_pin(fd, file_path);
   if (pinned < 0) {
     printf("Failed to pin map to the file system: %d (%s)\n", pinned,
--- code/chapter-3/map_pinning_fetch.c
+++ code/chapter-3/map_pinning_fetch.c
@@ -2,19 +2,21 @@
 #include <linux/bpf.h>
 #include <stdio.h>
 #include <string.h>
-#include "bpf.h"
+#include <bpf/bpf.h>
 
 static const char *file_path = "/sys/fs/bpf/my_array";
 
 int main(int argc, char **argv) {
   int fd, key, value, result;
 
+  // 读取字典的文件句柄
   fd = bpf_obj_get(file_path);
   if (fd < 0) {
     printf("Failed to fetch the map: %d (%s)\n", fd, strerror(errno));
     return -1;
   }
 
+  // 查询
   key = 1;
   result = bpf_map_lookup_elem(fd, &key, &value);
   if (result < 0) {
