c774f716889519340bb91600426c3e43f34ea539
root <QQGoblin>
2022-05-15 15:50:45 +0800

    kk

--- code/chapter-2/hello_world/bpf_program.c
+++ code/chapter-2/hello_world/bpf_program.c
@@ -1,14 +1,31 @@
 #include <linux/bpf.h>
 #define SEC(NAME) __attribute__((section(NAME), used))
 
-static int (*bpf_trace_printk)(const char *fmt, int fmt_size,
-                               ...) = (void *)BPF_FUNC_trace_printk;
+/**
+ * BPF_FUNC_trace_printk/BPF_FUNC_get_current_pid_tgid 是 linux/bpf.h 中，通过宏定义的 bpf 辅助函数 ID
+ * 
+ * 展开后，bpf_trace_printk的定义等价于：
+ * 
+ *    static long (*bpf_trace_printk)(const char *fmt, __u32 fmt_size, ...) = (void *) 6;  # void * 表示无类型指针
+ * 
+ * 在 bpf_helper_defs.h 中包含所有 bpf 辅助函数的定义，可以直接引用这个头文件从而省略这些定义。
+ * 
+ * 辅助函数的文档说明，可以参考：https://man7.org/linux/man-pages/man7/bpf-helpers.7.html
+ * 
+ */
+static long (*bpf_trace_printk)(const char *fmt, int fmt_size,...) = (void *)BPF_FUNC_trace_printk;
+static __u64 (*bpf_get_current_pid_tgid)(void) = (void *)BPF_FUNC_get_current_pid_tgid;
+
 
 SEC("tracepoint/syscalls/sys_enter_execve")
-int bpf_prog(void *ctx) {
-  char msg[] = "Hello, BPF World!";
-  bpf_trace_printk(msg, sizeof(msg));
+int bpf_prog_hello_world(void *ctx) {
+
+  int pid = bpf_get_current_pid_tgid() >> 32;
+  const char msg[] = "Hello, BPF World! My PID is %d\n";
+  // 该函数将输出打印到 /sys/kernel/debug/tracing/trace_pipe 
+  bpf_trace_printk(msg, sizeof(msg),pid);
   return 0;
+
 }
 
 char _license[] SEC("license") = "GPL";
--- code/chapter-2/hello_world/loader.c
+++ code/chapter-2/hello_world/loader.c
@@ -1,11 +1,16 @@
 #include "bpf_load.h"
 #include <stdio.h>
+#include "trace_helpers.h"
 
 int main(int argc, char **argv) {
+
+  // load_bpf_file 函数定义在 bpf_load.h 中， 5.10 以上版本的内核移除了上述文件。我们可以将该 bpf_load.h/bpf_loader.c 复制到当前目录。
   if (load_bpf_file("bpf_program.o") != 0) {
     printf("The kernel didn't load the BPF program\n");
     return -1;
   }
+
+
 
   read_trace_pipe();
 
